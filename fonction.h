cbe6b7a7e54ea22632eaa8731c288d26200fd933
nielsk29 <nielskeulen29@gmail.com>
2023-10-26 17:27:36 +0200

    envoie fichier

---
+++ fonction.h
@@ -0,0 +1,8 @@
+#include <time.h>
+#include <stdbool.h>
+
+bool test_carre(float* pos);
+
+float rayon(float direction);
+
+void image();
---
+++ variable.h
@@ -0,0 +1,13 @@
+#include <time.h>
+#include <stdbool.h>
+
+extern const int l_map[];
+extern const int taille_carre;
+extern const int taille_map[];
+extern const char* map;
+extern float pos_j[];
+extern float regard_j;
+extern const float angle_regard;
+extern const int nb_rayon;
+extern const int taille_ecran;
+int def_var();
